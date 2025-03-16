4add5824eda146fb17ebd58684e3e941bfa451bc
matheusgomes28 <matheusgarcia28@gmail.com>
2025-03-16 10:46:18 +0000

    testing

--- src/todoApp.tsx
+++ src/todoApp.tsx
@@ -17,14 +17,19 @@ const RenderItem = ({ item }: { item: String }) => {
 const ItemView = ({ text }: { text: String }) => {
     return <view
         // style={{ width: "80%" }}
-        className="flex bg-gray-500 flex-grow text-black border-l-8 border-green-500 rounded-md px-3 py-2 w-full md:w-5/12 lg:w-3/12"
+        className="flex bg-gray-200 flex-grow text-black border-l-8 border-green-500 rounded-md px-3 py-2"
     >
-        <text className="w-5/4">
-            { text }
-        </text>
-        <text className="rounded-2xl bg-red-600 px-4 py-2 font-bold leading-none text-white w-1/5">
-            Remove
-        </text>
+        <view className="w-4/5">
+            <text className="text-slate-800">
+                { text }
+            </text>
+        </view>
+
+        <view className="rounded-2xl bg-green-500 px-4 py-2 font-bold leading-none w-1/5">
+            <text className="text-white">
+                Rm
+            </text>
+        </view>
     </view>
 };
 
